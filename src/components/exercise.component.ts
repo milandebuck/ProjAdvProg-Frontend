eed78de5b9019e5896428c8ce4e3aea8db8b52c2
Milan De Buck <milan.debuck@student.odisee.be>
2016-12-04 16:32:58 +0100

    final html of exercisecomponent

--- src/components/exercise.component.ts
+++ src/components/exercise.component.ts
@@ -11,12 +11,15 @@ import { EntryService } from './../services/entry.service';
 })
 export class ExerciseComponent implements OnInit{
     entries: Array<any>;
+    answers: Array<any>;
+    curEntry: any;
     private error;
     constructor( private entryService:EntryService){
 
     };
     private lenght = 10;
     count = 0;
+    i = 0;
     private answers = [];
     ngOnInit(){
         console.log('initializing..');
@@ -26,9 +29,22 @@ export class ExerciseComponent implements OnInit{
     getEntries(count){
         this.entryService.getEntries()
             .subscribe(
-                entries => this.entries=entries,
+                (entries) => {
+                    this.entries=entries;
+                    this.curEntry = entries[0];
+                },
                 error => this.error = error
             )
+    }
+
+    next(answer){
+        if(this.count < this.entries.length){
+            this.count ++;
+            this.answers.push(answer);
+            this.curEntry = this.entries[this.count];
+        }
+        alert('test completed')
+
     }
 
     private correctExercise(){
