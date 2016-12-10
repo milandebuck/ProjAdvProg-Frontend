c1dbc5aad65e2af41518dd1d1adc3d635ec83a57
Domien Herdewel <domien.herdewel@student.odisee.be>
2016-12-10 13:15:05 +0100

    something, somewhere, over the rainbow

--- src/services/entry.service.js
+++ src/services/entry.service.js
@@ -1,4 +1,3 @@
-"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -75,6 +74,6 @@ var EntryService = (function () {
         __metadata('design:paramtypes', [http_1.Http, cookie_service_1.CookieService])
     ], EntryService);
     return EntryService;
-}());
+})();
 exports.EntryService = EntryService;
 //# sourceMappingURL=entry.service.js.map
