7e3c4f534379929a041cf27ca4682f13472b2a1f
Milan De Buck <milan.debuck@student.odisee.be>
2016-12-18 14:54:46 +0100

    added classcomponent

---
+++ src/components/class.component.ts
@@ -0,0 +1,22 @@
+import { Component ,OnInit} from '@angular/core';
+import {LoadingPage} from "./loading-indicator/loading-indicator";
+
+@Component({
+    selector: 'class',
+    template: require('./templates/class.component.html'),
+    styles: [ require('./styles/app.component.css'),require('./styles/class.component.css') ]
+})
+
+export class ClassComponent extends LoadingPage implements  OnInit{
+    username:string = localStorage.getItem('username');
+    classes:Array<any>;
+    constructor(){
+        super(true);
+    }
+
+    ngOnInit(){
+
+    }
+
+
+}
